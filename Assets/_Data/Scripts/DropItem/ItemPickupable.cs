40aafd6633f3fdaf43d95fcf3f02be018c2d329a
An Nguyen <nguyenvanan01102003@gmail.com>
2024-02-19 09:08:20 +0700

    lession 38

--- Assets/_Data/Scripts/DropItem/ItemPickupable.cs
+++ Assets/_Data/Scripts/DropItem/ItemPickupable.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,52 @@ public class ItemPickupable : SaiMonoBehaviour
 {
     [SerializeField] protected SphereCollider _collider;
 
+    public void OnMouseDown()
+    {
+        PlayerCtrl.Instance.PlayerPickup.ItemPickup(this);
+    }
+
+    #region Override
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadTrigger();
     }
+    #endregion
+
 
     protected virtual void LoadTrigger()
     {
         if (this._collider != null) return;
         this._collider = transform.GetComponent<SphereCollider>();
         this._collider.isTrigger = true;
-        this._collider.radius = 0.1f;
+        this._collider.radius = 1;
         Debug.LogWarning(transform.name + " LoadTrigger", gameObject);
     }
+    public ItemCode String2ItemCode(string itemName)
+    {
+        try
+        {
+
+            return (ItemCode)Enum.Parse(typeof(ItemCode), itemName);
+        }
+        catch (Exception e)
+        {
+            print(e);
+            return ItemCode.None;
+        }
+    }
+
+    public ItemCode GetItemCode()
+    {
+        return String2ItemCode(transform.parent.name);
+    }
+
+    public void Picked()
+    {
+        ItemDropSpawner.Instance.Despawn(transform.parent);
+    }
+
+
+
 }
